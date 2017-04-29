685e3a765e0229a6ef3cbe313d5f0c3279d54f67
ouroborous <lilitbakunts@gmail.com>
2017-04-29 18:37:03 +0400

    registration

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,7 +4,8 @@ import {
 	Text,
 	View,
 	TextInput,
-	ScrollView
+	ScrollView,
+  Picker
 } from 'react-native';
 
 import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
@@ -15,23 +16,84 @@ import Home from './Home';
 
 var radio_props = [
   {label: 'English', value: 1 },
-  {label: 'Russian', value: 0 },
-  {label: 'Armenian', value: 0 }
+  {label: 'Russian', value: 2 },
+  {label: 'Armenian', value: 3 }
 ];
 export default class Register extends Component {
 
-	constructor() {
-		super()
-		this.state = {value: 0}
+	constructor(props) {
+		super(props)
+		this.state = {
+      name: '',
+      surname: '',
+      ProfilePicURL: '',
+      email: '',
+      phone: '',
+      genderID: 1,
+      username: '',
+      password: '',
+      password_confirm: '',
+      languageID: 1,
+      errors: ''
+
+      // mode: Picker.MODE_DIALOG
+    }
+
 	}
 
-	press() {}
+  onValueChange(key, value) {
+    const newState = {}; 
+    newState[key] = value;
+    this.setState(newState);
+  }
+
+	async onRegisterPressed() {
+
+    try {
+  
+      let response = await fetch('http://192.168.137.1/gym/request/Auth.php', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            method: 'registration',
+            name: this.state.name,
+            surname: this.state.surname,
+            email: this.state.email,
+            phoneNumber: this.state.phone,
+            genderID: this.state.genderID,
+            username: this.state.username,
+            passcode: this.state.password,
+            password_confirm: this.state.password_confirm,
+            languageID: this.state.languageID
+                    
+        })
+      });
+
+      let res = await response.text();
+
+      if (res.result === 1) {
+        console.log('res success is: ' + res.message)
+      } else {
+
+        let formErrors = JSON.parse(res)
+        
+        this.setState({errors: formErrors.message})
+
+      }
+    } catch(errors) {
+      console.log('catch errors: ' + errors)
+      
+    }
+  }
 
 	render() {
 		return (
 			
 			<ScrollView style={styles.scroll}>
-
+       
 				<Button
 					onPress={() => {this.props.navigator.pop()}} 
 					styles={{button: styles.primaryButton, label: styles.buttonWhiteText}}
@@ -43,28 +105,97 @@ export default class Register extends Component {
           <RadioForm
             radio_props={radio_props}
             initial={0}
-            onPress={(value) => {this.setState({value:value})}}
+            onPress={(val) => {this.setState({languageID: val})}}
           />
         </Container>
         <Container>
           <Label text="Name" />
           <TextInput
-            secureTextEntry={true}
+            onChangeText={(val) => this.setState({name: val})}
             style={styles.textInput}
             placeholder="Name"
           />
           <Label text="Surname" />
           <TextInput
-            secureTextEntry={true}
+            onChangeText={(val) => this.setState({surname: val})}
             style={styles.textInput}
             placeholder="Surname"
           />
         </Container>
-          
+
+        <Container>
+          <Picker 
+            selectedValue={this.state.genderID}
+            onValueChange={(lang) => this.setState({genderID: lang})}
+            mode="dropdown"
+          > 
+              <Picker.Item label="Male" value="1" />
+              <Picker.Item label="Female" value="2" /> 
+          </Picker>
+        </Container>
+
+        <Container>
+          <Label text="Phone" />
+          <TextInput
+            onChangeText={(val) => this.setState({phone: val})}
+            style={styles.textInput}
+            placeholder="+374"
+          />
+        </Container>
+        <Container>
+          <Label text="Email address" />
+          <TextInput
+            onChangeText={(val) => this.setState({email: val})}
+            style={styles.textInput}
+            placeholder="name@domain.com"
+          />
+        </Container>
+        <Container>
+          <Label text="Username" />
+          <TextInput
+            onChangeText={(val) => this.setState({username: val})}
+            style={styles.textInput}
+            placeholder="username"
+          />
+        </Container>
+        <Container>
+          <Label text="Password" />
+          <TextInput
+            onChangeText={(val) => this.setState({password: val})}
+            secureTextEntry={true}
+            style={styles.textInput}
+          />
+           <TextInput
+            onChangeText={(val) => this.setState({password_confirm: val})}
+            secureTextEntry={true}
+            style={styles.textInput}
+          />
+
+        </Container>
+        <Container>
+          <Button
+            label="Register"
+            styles={{label: styles.buttonBlackText}}
+            onPress={this.onRegisterPressed.bind(this)} />
+        </Container>
+        <Container>
+          <Errors errors={this.state.errors} />
+        </Container>
+        
 			</ScrollView>
+
 
 		);
 	}
+}
+
+
+const Errors = (props) => {
+  return (
+    <View>
+      <Text style={styles.error}>{props.errors}</Text>
+    </View>
+  ) 
 }
 
 const styles = StyleSheet.create({
@@ -98,5 +229,11 @@ const styles = StyleSheet.create({
 	},
 	footer: {
 		marginTop: 100
-	}
+	},
+  
+  error: {
+    color: '#FFF',
+    backgroundColor: 'red',
+    marginTop: 100
+  }
 });
